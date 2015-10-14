41700cb75ae64d97c9bd804dda8ab22c643fa30e
Radu Nicolae <ranicolae@gmail.com>
2015-10-13 21:14:20 -0400

    bomb.js ready. testing time

--- bomb.js
+++ bomb.js
@@ -18,10 +18,10 @@
 
 function gen_bomb_list(messages){
 	var bomb_list = [];
-	for (i = 0; i < messages.length; i++){
-		if (messages[i].type == 'PLAYER_ACTION_BOMB')
-			bomb_list.push(messages[i].player);
-	}
+	messages.forEach(function(message) {
+		if (message.type == 'PLAYER_ACTION_BOMB')
+			bomb_list.push(message.player);
+	});
 
 	return bomb_list;
 }
@@ -39,6 +39,12 @@ function player_in_bomb(player_id, bomber_id) {
 	return Math.sqrt( a*a + b*b ) < 3;
 }
 
+function reset_player(player_id){
+	var new_pos = getUniquePosition(player_id);
+	globalState.players[player_id].x = new_pos.x;
+	globalState.players[player_id].y = new_pos.y;
+}
+
 function bomb(messages) {
 	// for all players,
 	//   for all bombs,
@@ -46,19 +52,20 @@ function bomb(messages) {
 	// messages structure:
 	// [{'type': 'PLAYER_ACTION_BOMB', 'player': '...'}, ...]
 
-	bomb_list = gen_bomb_list(messages);
+	var bomb_list = gen_bomb_list(messages);
 
 	for (var player_id in globalState.players) {
-		if (p.hasOwnProperty(player)) {
-			for (i = 0; i < bomb_list.length; i++) {
-				bomber_id = bomb_list[i];
-				if player_in_bomb(player_id, bomber_id) {
-					get_unique_position(player_id);
+		if (globalState.players.hasOwnProperty(player_id)) {
+			bomb_list.forEach(function(bomber_id){
+				if (player_in_bomb(player_id, bomber_id) && player_id != bomber_id) {
+					reset_player(player_id);
 				}
 
-				globalState.players[bomb_list[i]].hasBomb = fase;
-			}
+				// reset bomb to false
+				globalState.players[bomber_id].hasBomb = false;
+			});
 		}
 	}
 
 }
+
